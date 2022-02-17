ac2832fe8ae6573d8423271897bd5e9753ed4b57
Lars Kellogg-Stedman <lars@oddbit.com>
2022-02-17 16:46:17 -0500

    Add docstrings (and clean up unnecessary improts)

--- openstack.py
+++ openstack.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python
 
-import concurrent.futures
 import json
 import subprocess
 
 from functools import cache
-from collections import defaultdict
 
 
 def openstack(*args):
+    """Run `openstack` CLI.
+
+    Append `-f json` to whatever arguments we receive from the caller, and
+    parse the returned JSON before returning a result."""
+
     cmd = ["openstack"]
     cmd.extend(args)
     cmd.extend(["-f", "json"])
@@ -17,27 +20,33 @@ def openstack(*args):
 
 @cache
 def get_project(uuid):
+    """Return details about a single project"""
     return openstack("project", "show", uuid)
 
 
 @cache
 def get_flavor(uuid):
+    """Return details about a single flavor"""
     return openstack("flavor", "show", uuid)
 
 
 def list_servers():
+    """Return a list of nova servers (aka "instances")"""
     return openstack("server", "list", "--all")
 
 
 @cache
 def get_server(uuid):
+    """Return details about a single server"""
     return openstack("server", "show", uuid)
 
 
 def list_volumes():
+    """Return a list of cinder volumes"""
     return openstack("volume", "list", "--all")
 
 
 @cache
 def get_volume(uuid):
+    """Return details about a single volume"""
     return openstack("volume", "show", uuid)
